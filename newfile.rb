482dd1afd99bc9ebb909b286f90848d905420382
ryanlennon <ryanlennon07@gmail.com>
2016-04-30 17:08:41 -0500

    initial commit

---
+++ newfile.rb
@@ -0,0 +1,13 @@
+class NewFile
+
+  def self.open_file(file_input)
+    file_data = []
+    File.open(file_input, "r") do |file|
+      file.each_line do |line|
+        file_data << line
+      end
+    end
+    file_data
+  end
+
+end
